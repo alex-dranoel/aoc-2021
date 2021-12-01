d88b0d70c477c805546561a9bb4e48915119beec
Alexandre Léonard <alexandre.leonard@wooclap.com>
2021-12-01 13:01:15 +0100

    Use zoo rollsum

--- day01/R/main.R
+++ day01/R/main.R
@@ -2,3 +2,7 @@ val <- read.csv('input.txt', header = FALSE)$V1
 cat('Part 1:', sum(diff(val) > 0))
 n <- length(val)
 cat('Part 2:', sum(diff(val[1:(n-2)] + val[2:(n-1)] + val[3:n]) > 0))
+
+# or with zoo
+library(zoo)
+cat('Part 2:', sum(diff(rollsum(val, 3, align = 'right')) > 0))
